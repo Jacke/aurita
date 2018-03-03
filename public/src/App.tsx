580a25183e0faa57f46ba600af8550dbaa6be935
Stan <iamjacke@gmail.com>
2018-03-04 01:14:23 +0300

    Implement major logic for front-end without state storage, inject websockets, implement major functions for read and set map position

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,19 +1,74 @@
 import * as React from 'react';
 import './App.css';
+const defaultLatLng = {lat: -25.363, lng: 131.044}
+import GMap from './GMap'
+import axios from 'axios';
 
-const logo = require('./logo.svg');
+interface State {
+  localPosition: any;
+  globalPosition: any;
+}
+interface Props {}
 
-class App extends React.Component {
+class App extends React.Component<Props, State> {
+  connection: any;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {localPosition: {}, globalPosition: {} };
+    this.connection = new WebSocket('ws://localhost:9000/stream');
+    this.connection.onmessage = (evt:any) => { 
+      let newPosition = JSON.parse(evt.data)
+      // Update position from websocket only if client has the old position
+      if (this.state.localPosition.lat != newPosition.lat && this.state.localPosition.lng != newPosition.lng) {
+        this.setState({localPosition: {lat: newPosition.lat, lng: newPosition.lng},
+                      globalPosition: {lat: newPosition.lat, lng: newPosition.lng} });
+      }
+    };    
+  }
+
+  componentDidMount() {
+    axios.get(`/api/v1/mapPosition`)
+      .then(res => {
+        console.log('res: ', res);
+        this.setState({localPosition: res.data, globalPosition: res.data })
+    });
+  }
+
+  handleSave = (e: any) => {
+    this.connection.send(JSON.stringify(this.state.localPosition));
+    axios.post(`/api/v1/mapPosition/${this.state.localPosition.lat}/${this.state.localPosition.lng}`, {})
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  handleDefault = (e: any) => this.setState(prevState => ({localPosition: defaultLatLng, globalPosition: defaultLatLng }))
+  _onChange = (e: object) => this.setState({localPosition: e })
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React test</h1>
+          <img src='/assets/images/4d7.png' className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Aurita</h1>
+          <button onClick={this.handleDefault}>
+            Default position
+          </button>
+          <button onClick={this.handleSave}>
+            Save current
+          </button>                    
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <GMap 
+          onChange={this._onChange} 
+          globalPosition={this.state.globalPosition}
+          localPosition={this.state.localPosition}
+          defaultLatLng={defaultLatLng} />
+        <p className="App-intro"></p>
       </div>
     );
   }
